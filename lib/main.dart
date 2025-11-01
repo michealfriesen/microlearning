ea2e8e7648c46101ed13fb6fda5d5500ed215cb3
Jack Glass <jackadrianglass@gmail.com>
2025-11-01 14:37:38 -0600

    feat: Added simple usage of a file picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +11,41 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(home: MainState());
+  }
+}
+
+class MainState extends StatefulWidget {
+  @override
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends State<MainState> {
+  String? _filePath;
+
+  Future<void> _pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    if (result != null) {
+      File file = File(result.files.single.path!);
+	  _filePath = file.path;
+      print(file.path);
+      print(file.stat());
+    } else {
+      print("canceled input");
+      // User canceled the picker
+    }
+  }
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text("something something file something"),
+            ElevatedButton(onPressed: _pickFile, child: Text("Press me!")),
+          ],
         ),
       ),
     );
